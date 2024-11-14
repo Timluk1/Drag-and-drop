bbfb403f9c0a0ee1b2f715aed1a7ad21643f0f27
Timluk1 <main345242@gmail.com>
2024-11-14 16:53:25 +0300

    Init

--- index.js
+++ index.js
@@ -1,61 +1,47 @@
-const boxs = document.querySelectorAll(".box");
-const container = document.querySelector(".home-container");
-const mainContainer = document.querySelector(".main-container");
-let selected = null;
+class DragAndDrop {
+    constructor() {
 
-for (let box of boxs) {
-    box.addEventListener("dragstart", (e) => {
-        
-        selected = e.target;
-        e.target.classList.add("selected");
-    });
+    }
+}
 
-    box.addEventListener("dragend", () => {
-        selected.classList.remove("selected");
-        selected = null;
-    });
-
-    box.addEventListener("dragenter", (e) => {
-        if (selected && e.target !== selected) {
-            e.target.classList.add("drag-over");
+class Block {
+    constructor() {
+        this.state = {
+            text: ""
         }
-    });
+    }
 
-    box.addEventListener("dragleave", (e) => {
-        e.target.classList.remove("drag-over");
-    });
 }
 
-container.addEventListener("dragover", (e) => {
-    e.preventDefault();
-});
+class BuisnesLogic {
+    constructor() {
+        const button = document.querySelector(".button");
+        const input = document.querySelector(".input");
 
-container.addEventListener("drop", (e) => {
-    e.preventDefault();
-    const box = e.target.closest(".box");
-    if (box && box !== selected) {
-        container.insertBefore(selected, box);
-    } else if (selected) {
-        container.appendChild(selected);
+        this.state = {
+            button: button,
+            input: input,
+            text: "",
+        }
+        this.bindEvents();
     }
-    clearDragOver();
-});
 
-mainContainer.addEventListener("dragover", (e) => {
-    e.preventDefault();
-});
+    onClickButton(event) {
 
-mainContainer.addEventListener("drop", (e) => {
-    e.preventDefault();
-    if (selected) {
-        mainContainer.appendChild(selected);
     }
-    clearDragOver();
-});
 
-// Убираем все временные стили "drag-over"
-function clearDragOver() {
-    document.querySelectorAll(".drag-over").forEach(el => {
-        el.classList.remove("drag-over");
-    });
+    onChangeInput(event) {
+        this.state.text = event.target.value;
+    }
+
+    bindEvents() {
+        this.state.button.addEventListener("click", this.onClickButton.bind(this));
+        this.state.input.addEventListener("input", this.onChangeInput.bind(this));
+    }
 }
+
+function main() {
+
+}
+
+main();
